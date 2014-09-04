f1f1a30abbcb1afb91885c72f0f8b35849d69f98
Toni S <TeKanne774@gmail.com>
2014-09-04 22:00:31 +0200

    Change style

--- app/views/blog/view.blade.php
+++ app/views/blog/view.blade.php
@@ -10,6 +10,7 @@
 				<span><a class="btn btn-md btn-link" href="{{ URL::action('BlogController@edit', [$post->id]) }}">Edit</a></span>
 			@endif
 		</h1>
+		<hr>
 		<div class="post-body">{{ Markdown::parse($post->body) }}</div>
 		<span class="post-footer text-muted">Published on
 		<b>{{ date("l jS \of F Y", strtotime($post->created_at)) }}</b> by
